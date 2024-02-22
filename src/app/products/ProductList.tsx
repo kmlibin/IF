bd1ef3009702352e7a6cfca6079499cc00053c59
Kelli Libin <km.putnam2@gmail.com>
2024-02-22 15:43:37 -0700

    dummy data, generally show navbar and items

--- src/app/products/ProductList.tsx
+++ src/app/products/ProductList.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import quilt from '../../../public/quilt.jpg'
 
 interface ProductListProps {
     products: {
@@ -9,16 +11,16 @@ interface ProductListProps {
 }
 const ProductList = ({products}: ProductListProps) => {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full gap-4 justify-center items-center min-h-[70vh]">
       {products &&
         products.map((item: any) => (
           <div key={item.price} className="w-1/4 flex flex-col">
+            <Image src={quilt} alt="quilt"/>
             <div>{item.name}</div>
             <div>{item.type}</div>
             <div>{item.price}</div>
           </div>
         ))}
-      <div>home</div>
     </div>
   )
 }
