535ca192f9da70eb1f1160602d1ce47b94b7349f
Kelli Libin <km.putnam2@gmail.com>
2024-02-28 13:49:40 -0700

    adding comments

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -2,14 +2,18 @@
 import client from "../utils/paypal/client";
 import paypal from "@paypal/checkout-server-sdk";
 
+//responsible for initiating the creation of a paypal order
 export async function createOrder() {
   try {
+    //init paypal client
     const PaypalClient = client();
-    //This code is lifted from https://github.com/paypal/Checkout-NodeJS-SDK
+    //new request to create an order
+    //  OrdersCreateRequest() creates a POST request to /v2/checkout/orders
     const request = new paypal.orders.OrdersCreateRequest();
-
+    //can retern minimal or representation. minimal includes id, status, and hateoas links. repre returns the complete resource and current state of resource
     request.headers["Prefer"] = "return=representation";
     request.requestBody({
+      //capture payment immediately after customer makes the payment...also authorize, but
       intent: "CAPTURE",
       purchase_units: [
         {
@@ -17,11 +21,15 @@ export async function createOrder() {
             currency_code: "USD",
             value: "10.00",
           },
+
         },
       ],
     });
+
+    //execute request, await response from paypal
     const response = await PaypalClient.execute(request);
-    console.log(response)
+    console.log(response);
+    //if response isn't created (201)
     if (response.statusCode !== 201) {
       console.log("RES: ", response);
       return { error: "true" };
@@ -30,8 +38,9 @@ export async function createOrder() {
     // Your Custom Code for doing something with order
     // Usually Store an order in the database like MongoDB
 
-    const orderId = response.result.id; 
-console.log(orderId)
+    //extract orderID from the response, then return it
+    const orderId = response.result.id;
+
     return { success: "true", orderId };
   } catch (err) {
     console.log("Err at Create Order: ", err);
@@ -39,21 +48,26 @@ console.log(orderId)
   }
 }
 
-export async function payOrder(orderId: any) {
-  console.log('runs!')
-  //Capture order to complete payment
-  console.log(orderId)
+//responsible for capturing the payment
+export async function payOrder(orderId: string) {
+  console.log("runs!");
+  console.log(orderId);
+  //initialize client again
   const PaypalClient = client();
+  //new request to capture the specified order, using orderID
   const request = new paypal.orders.OrdersCaptureRequest(orderId);
+
+  //@ts-ignore
   request.requestBody({});
+
   const response = await PaypalClient.execute(request);
   if (!response) {
     return { error: "true" };
   }
-
+  console.log(response);
   // Your Custom Code to Update Order Status
   // And Other stuff that is related to that order, like wallet
   // Here I am updateing the wallet and sending it back to frontend to update it on frontend
-console.log('success')
+  console.log("success");
   return { success: "true" };
 }
