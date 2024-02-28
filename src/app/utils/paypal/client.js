5d001a9ffeac34f5b3c708c0f7bb0f87387836f4
Kelli Libin <km.putnam2@gmail.com>
2024-02-28 14:16:57 -0700

    added next_public

--- src/app/utils/paypal/client.js
+++ src/app/utils/paypal/client.js
@@ -4,7 +4,7 @@ const configureEnvironment = function () {
   const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
   const clientSecret = process.env.NEXT_PUBLIC_PAYPAL_SECRET
 
-  return process.env.NODE_ENV === 'production'
+  return process.env.NEXT_PUBLIC_NODE_ENV === 'production'
     ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
     : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret)
 }
