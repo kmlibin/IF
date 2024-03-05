671a126ea3b7dcd5f640012ab34a6a6cfca22403
Kelli Libin <km.putnam2@gmail.com>
2024-03-05 12:26:15 -0700

    render collect info, pass it set final address

--- src/app/cart/checkout/page.tsx
+++ src/app/cart/checkout/page.tsx
@@ -2,10 +2,12 @@
 
 import { useEffect, useState } from "react";
 import Summary from "@/app/components/Summary";
+import CollectInfo from "./CollectInfo";
 import { useCart } from "@/app/components/CartContext";
 
 export default function Checkout() {
   const [finalTotal, setFinalTotal] = useState(0);
+  const [finalAddress, setFinalAddress] = useState<string>("")
   const { state } = useCart();
   const { cart } = state;
   const {subtotal} = state;
@@ -14,7 +16,9 @@ export default function Checkout() {
 
   return (
     <div className="flex w-full">
-      <div className="flex flex-col w-3/5">div 1</div>
+      <div className="flex flex-col w-3/5">
+        <CollectInfo setFinalAddress={setFinalAddress} />
+      </div>
       <div className="flex flex-col w-2/5">
         <Summary cart={cart}  subtotal={subtotal} finalTotal={finalTotal} />
       </div>
