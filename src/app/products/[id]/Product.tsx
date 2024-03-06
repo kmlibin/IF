965015e78cb97d3cd33e031c0d8cf97f4ed4c1ad
Kelli Libin <km.putnam2@gmail.com>
2024-03-06 15:24:37 -0700

    add quantity, send as payload

--- src/app/products/[id]/Product.tsx
+++ src/app/products/[id]/Product.tsx
@@ -11,6 +11,8 @@ interface ProductData {
     type: string;
     name: string;
     id: string;
+    description: string;
+    quantity: number;
   };
 }
 
@@ -20,26 +22,33 @@ interface ProductProps {
 
 export default function Product({ product }: ProductProps) {
   const { state, dispatch } = useCart();
+  const [selectedQty, setSelectedQty] = useState(1)
 
   // const [cart, setCart] = useState<any>(null);
   console.log(product);
+  console.log(state)
 
   const addToCart = () => {
     dispatch({
       type: "ADD_TO_CART",
-      payload: { id: product.id, data: product.data },
+      payload: { id: product.id, data: product.data, quantity: selectedQty },
     });
   };
 
   const removeFromCart = () => {
     dispatch({
       type: "REMOVE_FROM_CART",
-      payload: { id: product.id, data: product.data },
+      payload: { id: product.id, data: product.data, quantity: 0 },
     });
   };
 
-  console.log(state);
-
+  //set up how many of an item are available...qty is set in backend
+  const setQuantity = [];
+  for (let i = 1; i <= product.data.quantity; i++) {
+    setQuantity.push(<option key={i} value={i}>{i}</option>)
+  }
+  
+console.log(selectedQty)
   return (
     <div>
       {product && (
@@ -47,7 +56,14 @@ export default function Product({ product }: ProductProps) {
           <div>
             <Image src={quilt} alt="quilt" height={200} width={200} />
             <p>{product.data.name}</p>
+            <p>{product.data.description}</p>
             <p>{product.data.price}</p>
+            <select
+              value={selectedQty}
+              onChange={(e) => setSelectedQty(Number(e.target.value))}
+            >
+              {setQuantity}
+            </select>
             <button onClick={addToCart}>Add to Cart</button>
             <button onClick={removeFromCart}>Remove From Cart</button>
           </div>
