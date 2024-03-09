674faac93586e96a2440e64b8618dc9a376810cc
Kelli Libin <km.putnam2@gmail.com>
2024-03-09 11:04:19 -0700

    render all necessary components and pass props, set shipping state

--- src/app/cart/checkout/page.tsx
+++ src/app/cart/checkout/page.tsx
@@ -1,14 +1,15 @@
-"use client";
-
-import { useEffect, useState } from "react";
+'use client'
+import { useState } from "react";
 import Summary from "@/app/components/Summary";
 import CollectInfo from "./CollectInfo";
 import { useCart } from "@/app/components/CartContext";
 import { PayPal } from "./PayPal";
 import ChooseShipping from "./ChooseShipping";
+import { handleAddressValidation } from "@/app/actions";
 
 export default function Checkout() {
   const [finalTotal, setFinalTotal] = useState(0);
+  const [shipping, setShipping] = useState<number | undefined>()
   const { state } = useCart();
   const { contactInfo } = state;
   const { cart } = state;
@@ -16,18 +17,20 @@ export default function Checkout() {
 
   // console.log(cart);
 
+  console.log(shipping)
   return (
     <div className="flex w-full">
       <div className="flex flex-col w-3/5">
-        <CollectInfo />
-        <ChooseShipping />
-        <PayPal finalTotal={finalTotal} cart={cart} contactInfo={contactInfo} />
+        <CollectInfo handleAddressValidation={handleAddressValidation} />
+        <ChooseShipping setShipping={setShipping}/>
+        <PayPal finalTotal={finalTotal} contactInfo={contactInfo} cart={cart}/>
       </div>
       <div className="flex flex-col w-2/5">
         <Summary
           cart={cart}
           subtotal={subtotal}
           finalTotal={finalTotal}
+          shipping={shipping}
           setFinalTotal={setFinalTotal}
         />
       </div>
