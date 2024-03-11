61512ca7c7ffa140355eb41df0772117b53aa3fe
Kelli Libin <km.putnam2@gmail.com>
2024-03-11 14:47:26 -0600

    checked prices in db in create order, returned errors, added types, added checks to qty in pay order

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,11 +1,20 @@
 "use server";
 import client from "../utils/paypal/client";
+import { revalidatePath } from "next/cache";
 import paypal from "@paypal/checkout-server-sdk";
-import { collection, addDoc, getDoc, updateDoc, doc } from "firebase/firestore/lite";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore/lite";
 import { db } from "@/app/firebase/config";
+import { CartItem } from "../types";
+import { fetchPricesFromFirebase } from "../firebase/queries";
+import { redirect } from "next/navigation";
 
 export const handleAddressValidation = async (userAddress: any) => {
-  "use server";
   const cleanedAddress = userAddress.replace(/,/g, "");
   console.log(cleanedAddress);
   try {
@@ -30,13 +39,50 @@ export const handleAddressValidation = async (userAddress: any) => {
   }
 };
 
-//remove what doesn't need to be returned!
-//type things correctly
-//handle errors
+//firebase queries:
+async function postOrderToFirebase(orderObject: any) {
+  try {
+    // Reference to the 'orders' collection
+    const ordersCollection = collection(db, "orders");
+
+    // Add a new document to the 'orders' collection with the order object
+    const docRef = await addDoc(ordersCollection, orderObject);
+
+    console.log("Order successfully added with ID:", docRef.id);
+  } catch (error) {
+    console.error("Error adding order to Firebase:", error);
+    throw error;
+  }
+}
 
 //responsible for initiating the creation of a paypal order
-export async function createOrder(orderTotal: any) {
+export async function createOrder(orderTotal: number, cart: CartItem[]) {
   try {
+    //first compare prices between cart and database
+    const prices = await fetchPricesFromFirebase(cart);
+    //if error was thrown from fetchprices
+    if ("error" in prices) {
+      return { error: prices.error };
+    }
+    // make sure prices in cart match prices from db
+    for (const item of cart) {
+      const itemId = item.id;
+      const itemPrice = prices[itemId];
+
+      if (itemPrice === undefined) {
+        return { error: `Price not found for item ${itemId} in the database` };
+      }
+
+      // compare the prices
+      if (itemPrice !== item.data.price) {
+        //means there is a mismatch between the database and cart
+        return {
+          error: `Price mismatch for item ${itemId}. Please refresh the page and try again.`,
+        };
+      }
+    }
+
+    //create paypal order
     //init paypal client
     const PaypalClient = client();
     //new request to create an order
@@ -63,113 +109,109 @@ export async function createOrder(orderTotal: any) {
     //if response isn't created (201)
     if (response.statusCode !== 201) {
       console.log("RES: ", response);
-      return { error: response.statusMessage };
+      return { error: `Error creating order: ${response.statusMessage}` };
     }
 
     //extract orderID from the response, then return it
     const orderId = response.result.id;
 
-    return { success: "true", orderId, orderTotal };
+    return { success: true, orderId, orderTotal };
   } catch (err) {
     console.log("Err at Create Order: ", err);
-    return { error: "true" };
-  }
-}
-
-async function postOrderToFirebase(orderObject: any) {
-  try {
-    // Reference to the 'orders' collection
-    const ordersCollection = collection(db, "orders");
-
-    // Add a new document to the 'orders' collection with the order object
-    const docRef = await addDoc(ordersCollection, orderObject);
-
-    console.log("Order successfully added with ID:", docRef.id);
-  } catch (error) {
-    console.error("Error adding order to Firebase:", error);
-    throw error; // Optional: Rethrow the error to handle it further up the call stack
+    return { error: "Error creating order, please try again later." };
   }
 }
 
 //NEED TO:
-//I need to split off functions for updating and posting to the database; hopefully can put in server actions
-//try/catch or if or something. ONLY post the order if the response is successful.
-//figure out how i want to handle errors for user
-//need to type things correctly
-//revalidate data somewhere since orders changed and product quantities changed. probably in server actions...?
-//paypal loadind button states
-//redirect user to order confirmation page on success
+
+//might need to put each product as static, so would need to revalidate the id of the product whose quantities changed
 //needs to send emails, one to the admin, one to the client
 
 //responsible for capturing the payment
 export async function payOrder(
   orderId: string,
-  email: any,
-  name: any,
-  address: any,
-  products: any,
-  orderTotal: any
-  
+  email: string,
+  name: string,
+  address: string,
+  products: CartItem[],
+  orderTotal: number
 ) {
   console.log("runs!");
-  console.log(`email ${email} name${name}`)
 
+  try {
+    //initialize client again
+    const PaypalClient = client();
+    //new request to capture the specified order, using orderID
+    const request = new paypal.orders.OrdersCaptureRequest(orderId);
+    //@ts-ignore
+    request.requestBody({});
 
-  // console.log(orderId);
-  //initialize client again
-  const PaypalClient = client();
-  //new request to capture the specified order, using orderID
-  const request = new paypal.orders.OrdersCaptureRequest(orderId);
-  //@ts-ignore
-  request.requestBody({});
+    const response = await PaypalClient.execute(request);
+    if (!response) {
+      return {
+        error:
+          "Error with payment. Please try again later or contact email@email to complete transaction",
+      };
+    }
+    //put quantity check in its own function?
+    //ONLY send to firebase if the response is completed
+    if (response.result && response.result.status == "COMPLETED") {
+      // update quantity of each purchased item in Firebase
+      for (const product of products) {
+        const productId = product.id;
+        const purchasedQuantity = product.quantity;
 
-  const response = await PaypalClient.execute(request);
-  if (!response) {
-    return { error: "true" };
+        // find the document in the products collection
+        const productDocRef = doc(db, "products", productId);
+
+        // get the product data and ultimately the quantity
+        const productDocSnapshot = await getDoc(productDocRef);
+        const currentQuantity = productDocSnapshot?.data()?.quantity;
+
+        //make sure there is enough in the database
+        if (purchasedQuantity > currentQuantity) {
+          return {
+            error: `${product.data.name} is not available at the requested quantity`,
+          };
+        }
+        // update the quantity in a variable. Math.max(0...ensures that the value CANNOT be negative!)
+        const updatedQuantity = Math.max(
+          0,
+          currentQuantity - purchasedQuantity
+        );
+
+        // pass the updated quantity to the document
+        await updateDoc(productDocRef, { quantity: updatedQuantity });
+      }
+
+      //create a new date in iso to send to fb
+      const paidAt = new Date();
+      const paidAtIso = paidAt.toISOString();
+
+      //might not need shipping address, paypal collects it and it displays on their UI.
+      const orderObject = {
+        PaypalPaymentId: orderId,
+        PayPalemail: response.result.payer.email_address,
+        CustomerName: name,
+        CustomerEmail: email,
+        CustomerAddress: address,
+        paidAt: paidAtIso,
+        orderTotal: orderTotal,
+        hasShipped: false,
+        shipMethod: "USPS Ground",
+        tracking: "none",
+        products,
+      };
+
+      await postOrderToFirebase(orderObject);
+    }
+
+    revalidatePath("/products");
+    return { success: true, orderId };
+  } catch (err) {
+    return {
+      error:
+        "Error processing payment. Please try again later or contact email@email to complete purchase",
+    };
   }
-
-   // update quantity of each purchased item in Firebase
-   for (const product of products) {
-    const productId = product.id;
-    const purchasedQuantity = product.quantity;
-
-    // find the document in the products collection
-    const productDocRef = doc(db, "products", productId);
-
-    // get the product data and ultimately the quantity
-    const productDocSnapshot = await getDoc(productDocRef);
-    const currentQuantity = productDocSnapshot?.data()?.quantity;
-
-    // update the quantity in a variable. Math.max(0...ensures that the value CANNOT be negative!)
-    const updatedQuantity = Math.max(0, currentQuantity - purchasedQuantity);
-
-    // pass the updated quantity to the document
-    await updateDoc(productDocRef, { quantity: updatedQuantity });
-  }
-
-  //need to send to firebase ONLY if response is 201 (or whatever it is..check)
- 
-//create a new date in iso to send to fb
-const paidAt = new Date();
-const paidAtIso = paidAt.toISOString();
-
-//might not need shipping address, paypal collects it and it displays on their UI.
-  const orderObject = {
-    PaypalPaymentId: orderId,
-    PayPalemail: response.result.payer.email_address,
-    CustomerName: name,
-    CustomerEmail: email,
-    CustomerAddress: address,
-    paidAt: paidAtIso,
-    orderTotal: orderTotal,
-    hasShipped: false,
-    shipMethod: "USPS Ground",
-    tracking: "none",
-    products,
-    
-  };
-
-  await postOrderToFirebase(orderObject);
-  console.log("success");
-  return { success: "true" };
 }
