06ea153a1a98e16998bdd39dac303f7dd9288346
Kelli Libin <km.putnam2@gmail.com>
2024-03-11 14:47:52 -0600

    redirect user on success, clear local storage/global state

--- src/app/cart/checkout/PayPal.tsx
+++ src/app/cart/checkout/PayPal.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
+import React, {useEffect} from "react";
 
 import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { createOrder, payOrder } from "../../actions/index";
-
-
+import { useCart } from "@/app/components/CartContext";
+import { useRouter } from "next/navigation";
 interface CartItem {
   id: string;
   data: {
@@ -25,10 +25,11 @@ type PayPalProps = {
   finalTotal: number;
   cart: CartItem[];
   contactInfo: ContactInfo;
-}
+};
 
 export const PayPal = ({ finalTotal, contactInfo, cart }: PayPalProps) => {
-
+  const {state, dispatch} = useCart()
+  const router = useRouter()
   const client = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "";
   const initialOptions = {
     clientId: client,
@@ -36,7 +37,8 @@ export const PayPal = ({ finalTotal, contactInfo, cart }: PayPalProps) => {
     intent: "capture",
   };
 
-  let orderTotal: any;
+  let orderTotal: number;
+
   //had to confirm that contact info existed in order to send the state in the onapprove. should probably do that for products, but there will be a lot of
   //reworking and conditional rendering as i finish the ui. just trying to get functionality at the moment
   return (
@@ -46,8 +48,8 @@ export const PayPal = ({ finalTotal, contactInfo, cart }: PayPalProps) => {
           <div>
             <PayPalButtons
               createOrder={async (data, actions) => {
-                let response = await createOrder(finalTotal);
-                if (response.success === "true") {
+                let response = await createOrder(finalTotal, cart);
+                if (response.success === true) {
                   orderTotal = response.orderTotal;
                   return response.orderId;
                 } else {
@@ -57,7 +59,7 @@ export const PayPal = ({ finalTotal, contactInfo, cart }: PayPalProps) => {
                 }
               }}
               onApprove={async (data, actions) => {
-                const products =  cart ;
+                const products = cart;
                 const { name, email, address } = contactInfo;
                 let response = await payOrder(
                   data.orderID,
@@ -67,8 +69,15 @@ export const PayPal = ({ finalTotal, contactInfo, cart }: PayPalProps) => {
                   products,
                   orderTotal
                 );
-                if (response && response.success === "true") {
+                if (response && response.success === true) {
                   console.log("Payment successful");
+                  dispatch({
+                    type: "CLEAR_CART",
+                  });
+                  dispatch({
+                    type: "CLEAR_INFO",
+                  });
+                  router.push(`/orders/${response.orderId}`)
                 } else {
                   console.error("Payment failed:", response.error);
                 }
