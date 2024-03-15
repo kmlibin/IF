4e8ea2337aa77c071fe6571f960b79d4411ddb30
Kelli Libin <km.putnam2@gmail.com>
2024-03-15 11:42:30 -0600

    worked fine on frontend, but kept getting error that setUser was undefined. really went through and typed it

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,26 +1,42 @@
-"use client"
+"use client";
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { auth } from "@/app/firebase/config";
 
-export const AuthContext = createContext<any>(null);
+interface AuthState {
+  user: {
+    token: string,
+    isAdmin: boolean
+  };
+  setUser: React.Dispatch<React.SetStateAction<any>>; // Function to set user state
+}
+
+export const AuthContext = createContext<AuthState | null>(null);
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+  const authContext = useContext(AuthContext);
+  return {
+    user: authContext?.user,
+    setUser: authContext?.setUser,
+  };
+};
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<any>(null);
+  console.log(user);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      setUser(authUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authState: AuthState = { user: user || {}, setUser: setUser || (() => {}) };
+
+  return (
+    <AuthContext.Provider value={authState}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const [user, setUser] = useState<any>(null);
-  
-    useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged((authUser) => {
-        setUser(authUser);
-      });
-  
-      return () => unsubscribe();
-    }, []);
-  
-    return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
-  };
-  
-
-  export default AuthProvider;
+export default AuthProvider;
