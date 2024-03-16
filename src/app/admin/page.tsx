3f09cc60c3babf3fbaccec80e00b09175139547a
Kelli Libin <km.putnam2@gmail.com>
2024-03-16 15:41:48 -0600

    makes fetch to validate token

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
+import { cookies } from "next/headers";
 
-import {cookies} from 'next/headers'
-import { parseCookies } from "nookies";
+type Props = {};
 
+async function page(props: Props) {
+  let auth = "false";
+  const fetchData = async () => {
+    const cookieStore = cookies();
+    const token = cookieStore.get("token");
 
-type Props = {}
+    try {
+      const res = await fetch("http://localhost:3000/api/auth", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          // Include the cookie in the request headers
+          "Set-Cookie": `${token?.value}`,
+        },
+      });
+      console.log(res.status);
+      if (res.status == 200) {
+      
+        auth = "true";
+        console.log(`auth = ${auth}`)
+      } else {
+        auth = "false";
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  fetchData();
 
-const page = (props: Props) => {
-    // const cookieStore = cookies()
-    // const currentUser = cookieStore.get('currentUser')
-    const cookies = parseCookies();
-    const accessToken = cookies.accessToken;
-    const currentUser = cookies.currentUser;
-
-    console.log("Access Token:", accessToken);
-    console.log("Current User:", currentUser);
   // Check if currentUser equals "admin"
-  if (currentUser !== undefined && currentUser === "admin") {
+  if (auth === "true") {
     return <div>Only admin can see this</div>;
   } else {
     return <div>You are not authorized to view this page</div>;
   }
-};
+}
 
-export default page
+export default page;
