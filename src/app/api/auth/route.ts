ad02aee969205342bcea91f65dc5a4285e7ebb17
Kelli Libin <km.putnam2@gmail.com>
2024-03-16 15:41:27 -0600

    generally works and verifies normal token

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,30 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getAuth } from "firebase-admin/auth";
-import { cookies } from "next/headers";
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  const cookieStore = cookies();
+import { getAuth} from "firebase-admin/auth";
+import { signInWithCustomToken } from "firebase/auth";
+import { cookies } from "next/headers";
+import {auth} from '../../firebase/config'
+import {initializeAdminApp} from '../../firebase/firebaseAdmin';
+
+export async function GET(req: NextRequest, res: NextResponse) { 
+  initializeAdminApp()
+  console.log(req.headers.getSetCookie())
+
+
+
+  // console.log(cookieStore)
+  // const body = await req.json()
+  // console.log(req.cookies)
+const token = req.headers.getSetCookie()
+    console.log(`token = ${token}`)
+ 
   try {
+    // console.log(body)
     // get token
-    const token = cookieStore.get("token");
-    console.log(token)
+    
     // verify token on fb admin
     if(token) {
-       const decodedToken = await getAuth().verifyIdToken(token.value); 
-   
+
+    //  const sign = await signInWithCustomToken(auth, token.toString())
+    //  console.log(`sign = ${sign}`)
+       const decodedToken = await getAuth().verifyIdToken(token.toString()); 
+
+   console.log(`decodedToken=${decodedToken}`)
     
 
     // Check if the decoded token contains the necessary claims (e.g., isAdmin)
-    if (decodedToken.admin === true) {
+    if (decodedToken) {
+      console.log('has admin')
       // User is authorized to access the protected route
-      return NextResponse.json({ status: 200 });
+      return NextResponse.json({ error: 'success' }, { status: 200 })
     } else {
+      console.log('no admin')
       // User is not authorized; return an error response
-      return NextResponse.json({ status: 401 });
+      return NextResponse.json({ error: 'failure' }, { status: 403 })
     } }
   } catch (error) {
     // Handle any errors that occur during token verification
     console.error("Error verifying JWT token:", error);
-    return NextResponse.json({ status: 500 });
+    return NextResponse.json({ error: 'failure' }, { status: 500 })
   }
 }
