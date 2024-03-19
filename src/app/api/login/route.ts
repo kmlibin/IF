fae24ee2f3cb87d87dde2e94326570dcdf5d6fb3
Kelli Libin <km.putnam2@gmail.com>
2024-03-19 12:19:47 -0600

    clean up login function, still troubleshooting errors

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,111 +1,65 @@
-import { Auth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { getDoc, doc } from "firebase/firestore/lite";
 import { db } from "@/app/firebase/config";
 import { auth } from "@/app/firebase/config";
 import { initializeAdminApp } from "../../firebase/firebaseAdmin";
-import { cookies } from "next/headers";
-import { setCookie } from "nookies";
-import nookies from "nookies";
+
 import { NextRequest, NextResponse } from "next/server";
-import { NextApiResponse } from "next";
+
 import { getAuth } from "firebase-admin/auth";
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  //neex to use firebase admin
   initializeAdminApp();
-  const cookieStore = cookies();
+
   const body = await req.json();
   const { email, password } = body;
 
+  //maybe put signinwith email and pass in its own try catch...? is that a thing?
   try {
-    const response = await signInWithEmailAndPassword(auth, email, password);
-    let admin = false;
 
-    // After successful login, get the user's information from the users collection
+    //seems to always trip the catch block if credentials don't work...
+    await signInWithEmailAndPassword(auth, email, password);
+
+    // after successful login, get the user's uid and use that to look them up in user collection
     if (auth.currentUser) {
       const uid = auth.currentUser.uid;
       const userDoc = await getDoc(doc(db, "users", uid));
 
+      //providing that user exists, look them up to see if they are admin
       if (userDoc.exists()) {
         const userData = userDoc.data();
         if (userData.isAdmin) {
-          admin = true;
-          // const customClaims = { admin: true };
-          //  custom token for the user
-          // const customToken = await getAuth().createCustomToken(uid)
-          // console.log(customToken)
-          // const customToken = await getAuth().createCustomToken(
-          //   uid,
-          //   customClaims
-          // );
-          // console.log(`customToken = ${customToken}`);
+          //generate a token
           const token = await auth.currentUser.getIdToken();
+          //set custom claims, which are then attached to the user object in firebase admin (admin true)
           await getAuth()
             .setCustomUserClaims(uid, { admin: true })
             .then(() => {
               console.log("custom claims set!");
             });
-          // Store auth token and set cookies
-
+          //pass the token as a secure cookie to the frontend...also lock out with urls, look up
           const cookieOptions = `token=${token}; Max-Age=86400; Path=/; Secure; HttpOnly; SameSite=Strict`;
-
-          return new Response("Hello, Next.js!", {
+          //send a status 200 upon success,a lso set the cookie
+          return new Response("Response", {
             status: 200,
             headers: { "Set-Cookie": cookieOptions },
           });
         }
       }
+    } else {
+      console.log('this was tripped')
+      return Response.json(
+        { error: "Please provide valid credentials" },
+        { status: 401 }
+      );
     }
   } catch (err) {
-    console.error(err);
-    return NextResponse.json({ err });
+    console.log(err)
+    console.log('the final block was tripped')
+    return Response.json(
+      { error: "Internal Error" },
+      { status: 500 }
+    );
   }
 }
-
-// interface StoreTokenRequest {
-//   token: string;
-//   admin: string;
-// }
-
-// export async function storeToken(request: StoreTokenRequest) {
-//   const cookieStore = cookies()
-//   await cookieStore.set(null, 'fromGetInitialProps', request.token, {
-//     maxAge: 30 * 24 * 60 * 60,
-//     path: '/',
-//   })
-//   nookies.set(null, 'fromGetInitialProps2', request.admin, {
-//     maxAge: 30 * 24 * 60 * 60,
-//     path: '/',
-//   })
-// }
-
-// export async function login(email: string, password: string) {
-//   try {
-//     const response = await signInWithEmailAndPassword(auth, email, password);
-//     let admin;
-//     //after successful login, get the users information from the users collection, stored by uid
-//     console.log(`currentUser : ${auth.currentUser}`);
-//     if (auth.currentUser) {
-//       const uid = auth.currentUser.uid;
-//       const userDoc = await getDoc(doc(db, "users", uid));
-
-//       if (userDoc.exists()) {
-//         const userData = userDoc.data();
-
-//         //set admin to true so can return to frontend
-//         if (userData.isAdmin) {
-//           admin = true;
-//         }
-//       }
-
-//       // store auth token and store it in local storage
-//       const token = await auth.currentUser.getIdToken();
-
-//       storeToken({token: uid, admin: "admin"});
-//       return { token, admin };
-//     }
-//     console.log("Login success!");
-//   } catch (error: any) {
-//     console.log("Login failed:", error.message);
-//     return error.message;
-//   }
-// }
