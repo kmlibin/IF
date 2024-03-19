4be0ef3035031a66b21af1d1e6907a2d3c9929be
Kelli Libin <km.putnam2@gmail.com>
2024-03-19 11:39:23 -0600

    call the SA that calls the authentication

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,39 +1,16 @@
 import React from "react";
 import { cookies } from "next/headers";
+import { authUser } from "../actions";
 
 type Props = {};
 
 async function page(props: Props) {
-  let auth = "false";
-  const fetchData = async () => {
-    const cookieStore = cookies();
-    const token = cookieStore.get("token");
 
-    try {
-      const res = await fetch("http://localhost:3000/api/auth", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          // Include the cookie in the request headers
-          "Set-Cookie": `${token?.value}`,
-        },
-      });
-      console.log(res.status);
-      if (res.status == 200) {
-      
-        auth = "true";
-        console.log(`auth = ${auth}`)
-      } else {
-        auth = "false";
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  fetchData();
+  const result = await authUser();
+ 
 
   // Check if currentUser equals "admin"
-  if (auth === "true") {
+  if (result == true) {
     return <div>Only admin can see this</div>;
   } else {
     return <div>You are not authorized to view this page</div>;
